5732c0829d53be2b49e782196e53dad5a215cc15
Fabricio Garcia <gael.fabricio@gmail.com>
2019-10-31 19:58:37 -0600

    Add some tests for Player class

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,27 +1,29 @@
+# frozen_string_literal: true
+
 require_relative '../lib/classes.rb'
 
 describe Board do
+  before(:each) do
+    @board_test = Board.new
+    @player_test = Player.new
+  end
 
-    before(:each) do
-        @board_test = Board.new
+  describe '#initialize' do
+    it 'when board is properly set' do
+      expect(@board_test.game_board).to eql([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
     end
+  end
 
-   describe "#initialize" do
-    it "when board is properly set" do
-        expect(@board_test.game_board).to eql([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
+  describe '#invalid_move(choice)' do
+    it 'when user enter a valid number' do
+      expect(@board_test.invalid_move(2)).to be(false)
     end
-   end
+  end
 
-   describe "#invalid_move(choice)" do
-    it "when user enter a valid number" do
-        expect(@board_test.invalid_move(2)).to be(false)
+  describe '#invalid_move(choice)' do
+    xit 'when user enter an invalid number' do
+      expect(@board_test.invalid_move(20)).to be(true)
     end
-   end
-
-   describe "#invalid_move(choice)" do
-    xit "when user enter an invalid number" do
-        expect(@board_test.invalid_move(20)).to be(false)
-    end
-   end
+  end
 
 end
