8f330c299bff4b53028e57e8c731038a48b32f28
Fabricio Garcia <gael.fabricio@gmail.com>
2019-11-01 20:36:25 -0600

    Add more test cases to RSpec file

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,9 +1,31 @@
-require_relative '../lib/classes.rb'
+require_relative '../lib/classes'
 
 describe Board do
-    @board = Board.new
-    it "checks valid user input" do
-        @game_board = [" "," "," "," "," "," "," "," "," "," "," "]
-        expect(@board.invalid_move).to eql(true)
-    end
+
+	before(:example) do
+		@board = Board.new
+		@player = Player.new
+	end
+
+	describe '#initialize' do
+		it "checks it board is created" do
+			expect(@board.game_board).to match_array([" "," "," "," "," "," "," "," "," "," "," "])
+		end
+	end
+
+	context 'when initialized helper array' do
+		it 'helper array is empty' do
+			expect(@board.board_num).to match_array([])
+		end
+	end
+
+	describe 'Player' do
+		it 'when @choice initialize' do
+			expect(@player.choice).to be(nil)
+		end
+		it 'when @player initialize' do
+			expect(@player.player).to eq('X')
+		end
+	end
+
 end
